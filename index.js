093d2db0933894f3ab5ed1fddd286c84468f7d1f
Yoon-CH <changh950@gmail.com>
2022-11-16 13:28:43 +0900

    문제 남김 예시

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
-const input = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
+const input = fs.readFileSync("./dev/stdin").toString().trim().split(" "); // \n or " " or ""
+
+////////////////////////////////////////
 
 const first = input[0].split("").join("");
 const second = input[1].split("");
@@ -8,26 +10,15 @@ const a = Number(first) * Number(second[2]);
 const b = Number(first) * Number(second[1]);
 const c = Number(first) * Number(second[0]);
 const d = a + b * 10 + c * 100;
-// .map((item, i) => {
-//   console.log(item);
-//   return Number(item) * Number(second[2]);
-// });
+
 console.log(`${a}\n${b}\n${c}\n${d}`);
 
-// const chess = [1, 1, 2, 2, 2, 8];
+////////////////////////////////////////
 
-// const answer = input.map((i, index) => {
-//   return chess[index] - i;
-// });
+const chess = [1, 1, 2, 2, 2, 8];
 
-// // console.log(...answer);
+const answer = input.map((i, index) => {
+  return chess[index] - i;
+});
 
-// a = Number(input[0]);
-// b = Number(input[1]);
-// c = Number(input[2]);
-
-// // console.log((5 + 8) % 4);
-// console.log(Math.floor((a + b) % c));
-// console.log(((a % c) + (b % c)) % c);
-// console.log(Math.floor((a * b) % c));
-// console.log(Math.floor(((a % c) * (b % c)) % c));
+console.log(...answer);
